6e714e449e55ea33f860c3461d3b4daa3eebcbe4
jamieorc <jamie@dang.com>
2010-10-28 09:22:48 -0400

    * src/sicp_in_clojure/ch01.clj: Exercise 1.3.

--- src/sicp_in_clojure/ch01.clj
+++ src/sicp_in_clojure/ch01.clj
@@ -1,2 +1,9 @@
 (ns sicp-in-clojure.ch01)
 
+;; Exercise 1.3.
+;; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+(defn add-squares-of-2-largest-of-3 [x y z]
+  (let [a (if (> x y) x y)
+	b (if (> x z) x z)]
+    (+ (* a a) (* b b))))
+
