7cb07bc940897a7d06d8257dcd313f2910392f14
jamieorc <jamie@dang.com>
2010-12-01 08:34:39 -0500

    Added recursive and iterative functions for exercise 1.11.

--- src/sicp_in_clojure/ch01.clj
+++ src/sicp_in_clojure/ch01.clj
@@ -22,4 +22,29 @@
 	(float result)
 	(recur x result)))))
 	       
-    
+;; Exercise 1.11.  A function f is defined by the rule that
+;;   f(n) = n if n < 3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3.
+;; Write a procedure that computes f by means of a recursive process.
+;; Write a procedure that computes f by means of an iterative process.
+;; 1:(1) 1
+;; 2:(2) 2
+;; 3:(4) 2 + 2*1
+;; 4:(11) f(3) + 2*2 + 3*1
+;;         4     4     3
+;; 5:(25) f(4) + 2*f(3) + 3*2
+;;         11    8        6
+;; 6:(59) f(5) + 2*f(4) + 3*f(3)
+;;         25    22       12
+;; 7:(142) f(6) + 2*f(5) + 3*f(4)
+;;          59    50       33
+
+(defn ex-1-11-recursive [n]
+  (if (< n 3)
+    n
+    (+ (ex-1-11-recursive (- n 1)) (* 2 (ex-1-11-recursive (- n 2))) (* 3 (ex-1-11-recursive (- n 3))))))
+
+(defn ex-1-11-iterative [n]
+  (loop [x 2 y 2 z 0 count n]
+    (if (= count 2)
+      x
+      (recur (+ x y z) (* 2 x) (* (/ y 2) 3) (- count 1)))))
