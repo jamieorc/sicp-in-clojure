b5db9cc5221afb8d65eca1cedd92d2999e473f7f
jamieorc <jamie@dang.com>
2010-11-30 22:44:38 -0500

    Added find-cube-root.

--- src/sicp_in_clojure/ch01.clj
+++ src/sicp_in_clojure/ch01.clj
@@ -1,4 +1,5 @@
-(ns sicp-in-clojure.ch01)
+(ns sicp-in-clojure.ch01
+  (:require [clojure.contrib.math :as math]))
 
 ;; Exercise 1.3.
 ;; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
@@ -8,4 +9,17 @@
 	c (if (> b z) b z)]
     (+ (* a a) (* c c))))
 
-
+;; Exercise 1.8.
+;; Create a function to implement Newton's method for cube roots, where
+;; y is an approximation of the cube root:
+;; ((x/y*y) + 2y) / 3
+(defn find-cube-root [num, precision]
+  (loop [x num
+	 y 1]
+    (let [result (float (/ (+ (* 2 y) (float (/ x (* y y)))) 3))]
+      (println (str "result: " (float result)))
+      (if (> precision (math/abs (- result y)))
+	(float result)
+	(recur x result)))))
+	       
+    
